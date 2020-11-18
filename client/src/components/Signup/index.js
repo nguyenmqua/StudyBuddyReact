464b6d574b832614be6f791ac36906f51f67ba25
Joel Terrell <falconreid@hotmail.com>
2020-11-17 17:29:20 -0700

    styling for pages

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from "react";
 import {
   Button,
   Form,
@@ -7,9 +7,9 @@ import {
   Input,
   FormText,
   Alert,
-} from 'reactstrap';
-import { Link } from 'react-router-dom';
-import UserContext from '../../utils/UserContext';
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import UserContext from "../../utils/UserContext";
 
 const Signup = () => {
   const {
@@ -24,7 +24,7 @@ const Signup = () => {
   const [validUserName, setValidUserName] = useState(false);
   const [validPassword, setValidPassword] = useState(false);
   const [isConfirmed, setIsConfirmed] = useState(false);
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState({});
 
   useEffect(() => {
@@ -41,16 +41,16 @@ const Signup = () => {
     const length = userData.firstname.length;
     if (length === 0) {
       setValidFirstName(false);
-      setErrorMessage({ ...errorMessage, firstname: '' });
+      setErrorMessage({ ...errorMessage, firstname: "" });
     } else if (length < 3) {
       setValidFirstName(false);
       setErrorMessage({
         ...errorMessage,
-        firstname: 'First Name should be at least 3 characters.',
+        firstname: "First Name should be at least 3 characters.",
       });
     } else {
       setValidFirstName(true);
-      setErrorMessage({ ...errorMessage, firstname: '' });
+      setErrorMessage({ ...errorMessage, firstname: "" });
     }
   };
 
@@ -59,38 +59,38 @@ const Signup = () => {
     const length = userData.lastname.length;
     if (length === 0) {
       setValidLastName(false);
-      setErrorMessage({ ...errorMessage, lastname: '' });
+      setErrorMessage({ ...errorMessage, lastname: "" });
     } else if (length < 3) {
       setValidLastName(false);
       setErrorMessage({
         ...errorMessage,
-        lastname: 'First Name should be at least 3 characters.',
+        lastname: "First Name should be at least 3 characters.",
       });
     } else {
       setValidLastName(true);
-      setErrorMessage({ ...errorMessage, lastname: '' });
+      setErrorMessage({ ...errorMessage, lastname: "" });
     }
   };
 
   // uses regex to check is email is valid
   const checkEmail = () => {
     const validEmail = new RegExp(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
     const valid = validEmail.test(userData.email);
     const length = userData.email.length;
     if (length === 0) {
       setValidEmail(false);
-      setErrorMessage({ ...errorMessage, email: '' });
+      setErrorMessage({ ...errorMessage, email: "" });
     } else if (!valid) {
       setValidEmail(false);
       setErrorMessage({
         ...errorMessage,
-        email: 'Please enter a valid email address.',
+        email: "Please enter a valid email address.",
       });
     } else {
       setValidEmail(true);
-      setErrorMessage({ ...errorMessage, email: '' });
+      setErrorMessage({ ...errorMessage, email: "" });
     }
   };
 
@@ -99,38 +99,38 @@ const Signup = () => {
     const length = userData.username.length;
     if (length === 0) {
       setValidUserName(false);
-      setErrorMessage({ ...errorMessage, username: '' });
+      setErrorMessage({ ...errorMessage, username: "" });
     } else if (length < 5) {
       setValidUserName(false);
       setErrorMessage({
         ...errorMessage,
-        username: 'Username should be at least 5 characters.',
+        username: "Username should be at least 5 characters.",
       });
     } else {
       setValidUserName(true);
-      setErrorMessage({ ...errorMessage, username: '' });
+      setErrorMessage({ ...errorMessage, username: "" });
     }
   };
 
   // checks is password meets regex test (at least 8 letters, 1 capital and 1 number)
   const checkPassword = () => {
     const strongPassword = new RegExp(
-      /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/,
+      /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/
     );
     const valid = strongPassword.test(userData.password);
     const length = userData.password.length;
     if (length === 0) {
       setValidPassword(false);
-      setErrorMessage({ ...errorMessage, password: '' });
+      setErrorMessage({ ...errorMessage, password: "" });
     } else if (!valid) {
       setValidPassword(false);
       setErrorMessage({
         ...errorMessage,
-        password: 'Password should be at least 8 letters, 1 capital & 1 number',
+        password: "Password should be at least 8 letters, 1 capital & 1 number",
       });
     } else {
       setValidPassword(true);
-      setErrorMessage({ ...errorMessage, password: '' });
+      setErrorMessage({ ...errorMessage, password: "" });
     }
   };
 
@@ -138,18 +138,18 @@ const Signup = () => {
   const checkConfirmPassword = () => {
     if (confirmPassword.length === 0) {
       setIsConfirmed(false);
-      setErrorMessage({ ...errorMessage, confirmPassword: '' });
+      setErrorMessage({ ...errorMessage, confirmPassword: "" });
     } else if (
-      userData.password !== '' &&
+      userData.password !== "" &&
       userData.password === confirmPassword
     ) {
       setIsConfirmed(true);
-      setErrorMessage({ ...errorMessage, confirmPassword: '' });
+      setErrorMessage({ ...errorMessage, confirmPassword: "" });
     } else {
       setIsConfirmed(false);
       setErrorMessage({
         ...errorMessage,
-        confirmPassword: 'Passwords must match',
+        confirmPassword: "Passwords must match",
       });
     }
   };
@@ -172,7 +172,7 @@ const Signup = () => {
             onBlur={checkFirstname}
             valid={validFirstName}
           />
-          <FormText>{errorMessage['firstname']}</FormText>
+          <FormText>{errorMessage["firstname"]}</FormText>
         </FormGroup>
         <FormGroup>
           <Label for="lastname">Last Name</Label>
@@ -186,7 +186,7 @@ const Signup = () => {
             onBlur={checkLastname}
             valid={validLastName}
           />
-          <FormText>{errorMessage['lastname']}</FormText>
+          <FormText>{errorMessage["lastname"]}</FormText>
         </FormGroup>
         <FormGroup>
           <Label for="email">Email</Label>
@@ -200,7 +200,7 @@ const Signup = () => {
             onBlur={checkEmail}
             valid={validEmail}
           />
-          <FormText>{errorMessage['email']}</FormText>
+          <FormText>{errorMessage["email"]}</FormText>
         </FormGroup>
         <FormGroup>
           <Label for="username">Username</Label>
@@ -214,7 +214,7 @@ const Signup = () => {
             onBlur={checkUsername}
             valid={validUserName}
           />
-          <FormText>{errorMessage['username']}</FormText>
+          <FormText>{errorMessage["username"]}</FormText>
         </FormGroup>
         <FormGroup>
           <Label for="password">Password</Label>
@@ -228,7 +228,7 @@ const Signup = () => {
             onBlur={checkPassword}
             valid={validPassword}
           />
-          <FormText>{errorMessage['password']}</FormText>
+          <FormText>{errorMessage["password"]}</FormText>
         </FormGroup>
         <FormGroup>
           <Label for="confirmPassword">Confirm Password</Label>
@@ -242,7 +242,7 @@ const Signup = () => {
             onKeyUp={checkConfirmPassword}
             valid={isConfirmed}
           />
-          <FormText>{errorMessage['confirmPassword']}</FormText>
+          <FormText>{errorMessage["confirmPassword"]}</FormText>
         </FormGroup>
         {/* if all fields are valid, allow the user to submit the form */}
         {validFirstName &&
@@ -260,7 +260,7 @@ const Signup = () => {
           </Button>
         )}
         <p className="signupLink">
-          <Link to="/login">already have an account? Sign in here</Link>
+          <Link to="/login">Already have an account? Sign in here</Link>
         </p>
       </Form>
     </div>
