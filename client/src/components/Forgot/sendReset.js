ae37014ad3187af21f82f2f4c65611189b707c3a
phantomramen <ali.hernandez9two@gmail.com>
2020-11-21 13:49:22 -0700

    reinstalled

--- client/src/components/Forgot/sendReset.js
+++ client/src/components/Forgot/sendReset.js
@@ -12,29 +12,18 @@ const SendReset = () => {
       <hr />
       <Form>
         <FormGroup>
-          <Label for="username">Username</Label>
+          <Label for="email">Email Adress:</Label>
           <Input
             type="text"
-            name="username"
-            id="username"
-            placeholder="username"
-            value={userData.username}
-            onChange={handleInputChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="password">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="password"
-            value={userData.password}
+            name="email"
+            id="email"
+            placeholder="email"
+            value={userData.email}
             onChange={handleInputChange}
           />
         </FormGroup>
         <Button onClick={handleLogin} color="primary" block>
-          Login
+          Send Reset Email
         </Button>
         <p className="signupLink">
           <Link to="/signup">Don't have an account? Sign up here</Link>
