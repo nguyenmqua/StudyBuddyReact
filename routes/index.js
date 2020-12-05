970ddc80cb35c63d423fd8a4602667f73dc4f129
phantomramen <ali.hernandez9two@gmail.com>
2020-12-05 11:19:31 -0700

    proxy error

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const sendMailRoutes= require("./sendMail")
+
 // API Routes
 router.use("/api", apiRoutes);
+
+router.use("/", sendMailRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
--- routes/sendMail.js
+++ routes/sendMail.js
@@ -1,15 +1,10 @@
 const router = require("express").Router("")
 const { get } = require("mongoose");
 const welcomemailerController = require("../controllers/welcomemailerController");
-// const withAuth = require('../config/middleware/middleware');
 
 router.route("/sendMail")
   .post(welcomemailerController.sendMail);
 
-//   router.route("/checkToken/:token")
-//        .get(withAuth, nodemailerController.checkToken)
 
-
-// router.route("/protected").get(nodemailerController.sendJWT);
 
 module.exports = router;
