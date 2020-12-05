4e0416904a6aaf786a761e9499ff2c66957527ba
Joel Terrell <falconreid@hotmail.com>
2020-12-05 13:17:55 -0700

    styling change to landing page text

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -6,18 +6,17 @@ module.exports = {
     db.Post.find({ subject: req.params.id })
       .populate("userId")
       .then((DBpost) => {
-        console.log("DBpost: ", DBpost);
         res.json(DBpost);
       })
       .catch((err) => res.status(422).json(err));
   },
 
   subject: function (req, res) {
-    console.log(req.body);
+    // console.log(req.body);
     db.Post.find({})
       .populate("userId")
       .then((DBpost) => {
-        console.log(DBpost);
+        // console.log(DBpost);
         res.json(DBpost);
       })
       .catch((err) => res.status(422).json(err));
