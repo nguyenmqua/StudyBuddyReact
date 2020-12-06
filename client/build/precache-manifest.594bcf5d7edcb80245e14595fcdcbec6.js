64cf9734b41dda49f0e41bea2a0c42180d12cb60
Qua Nguyen <Nguyenmqua@gmail.com>
2020-12-06 15:16:01 -0700

    deploying

---
+++ client/build/precache-manifest.594bcf5d7edcb80245e14595fcdcbec6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef408ee948b3aed7bc4ed2352fedeb29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02eb50e7bf807a56b4c3",
+    "url": "/static/css/main.91e49bb7.chunk.css"
+  },
+  {
+    "revision": "40ad199063f9037b194b",
+    "url": "/static/js/2.94b10eeb.chunk.js"
+  },
+  {
+    "revision": "09151ff12aba5c289140d69923769826",
+    "url": "/static/js/2.94b10eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02eb50e7bf807a56b4c3",
+    "url": "/static/js/main.8df7e960.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
--- server.js
+++ server.js
@@ -42,6 +42,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/studybuddy', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
+  useFindAndModify: false
 });
 
 
