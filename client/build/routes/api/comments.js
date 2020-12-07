c3cf2fbc7f8ee5086df26890650e4a2d73207928
Qua Nguyen <Nguyenmqua@gmail.com>
2020-12-07 08:55:42 -0700

    deploy

--- client/build/routes/api/comments.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const commentsController = require("../../controllers/commentsController");
-
-router.route("/")
-  .post(commentsController.create);
-
-module.exports = router;
--- client/build/routes/api/newsfeed.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const newsfeedController = require("../../controllers/newsfeedController");
-
-router.route("/")
-  .get(newsfeedController.get);
-
-module.exports = router;
